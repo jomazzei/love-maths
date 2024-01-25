44134822f9bafcd7c7c89ae0738751bb74e355c9
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-01-25 14:56:30 +0000

    added question display addition, and error display

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,31 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
-function runGame() {
+/**
+ * The main game "loop", called when script is loaded
+ * and after answers have been processed
+ */
 
+function runGame(gameType) {
+
+    // Create the random numbers for game
+    let num1 = Math.ceil(Math.random() * 25);
+    let num2 = Math.ceil(Math.random() * 25);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -36,8 +53,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
